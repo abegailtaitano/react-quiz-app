4d8215e8fcbb89792d6d534fdf9d2b47f317df01
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-02-23 22:00:50 -0600

    completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,128 +1,133 @@
-import React, { Component } from 'react'
-import {QuizData} from './QuizData'
-import './App.css'
+import React, { Component } from "react";
+import { QuizData } from "./QuizData";
+import "./App.css";
 
 export class Quiz extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-        userAnswer:null, //current users answer
-        currentIndex:0,  //current questions index
-        options: [],     //the four options
-        quizEnd: false,  //determines if it's the last question
-        score: 0,        //holds the score
-        disabled: true   // determines the status of the buttons
-    }
-}
-  
+      userAnswer: null, //current users answer
+      currentIndex: 0, //current questions index
+      options: [], //the four options
+      quizEnd: false, //determines if it's the last question
+      score: 0, //holds the score
+      disabled: true, // determines the status of the buttons
+    };
+  }
+
   //Component that holds the current quiz
-loadQuiz = () => {
-    const {currentIndex} = this.state //get the current question index
+  loadQuiz = () => {
+    const { currentIndex } = this.state; //get the current question index
     this.setState(() => {
+      return {
+        question: QuizData[currentIndex].question,
+        options: QuizData[currentIndex].options,
+        answer: QuizData[currentIndex].answer,
+      };
+    });
+  };
+
+  nextQuestionHandler = () => {
+    const { userAnswer, answer, score } = this.state;
+
+    if (userAnswer === answer) {
+      this.setState({
+        score: score + 1,
+      });
+    }
+    this.setState({
+      currentIndex: this.state.currentIndex + 1,
+      userAnswer: null,
+    });
+  };
+  componentDidMount() {
+    this.loadQuiz();
+  }
+
+  //Check the answer
+  checkAnswer = (answer) => {
+    console.log(answer);
+    this.setState({
+      userAnswer: answer,
+      disabled: false,
+    });
+  };
+
+  componentDidUpdate(_prevProps, prevState) {
+    const { currentIndex } = this.state;
+    if (this.state.currentIndex != prevState.currentIndex) {
+      this.setState(() => {
         return {
-            question: QuizData[currentIndex].question,
-            options : QuizData[currentIndex].options,
-            answer: QuizData[currentIndex].answer          
-        }
-    })
-}
-
-nextQuestionHandler = () => {
-    const {userAnswer, answer, score} = this.state
-     if(userAnswer === answer){
-        this.setState({
-            score: score + 1
-        })
-        console.log(score)
+          question: QuizData[currentIndex].question,
+          options: QuizData[currentIndex].options,
+          answer: QuizData[currentIndex].answer,
+        };
+      });
     }
-  
-    //Check if correct answer and increment score
-   
-    this.setState({
-        currentIndex: this.state.currentIndex + 1,
-        userAnswer: null
-    })
-}
+  }
 
-    componentDidMount() {
-        this.loadQuiz();
+  finishHandler = () => {
+    const { userAnswer, answer, score } = this.state;
+    if (userAnswer === answer) {
+      this.setState({
+        score: score + 1, //adds 1 to end score
+      });
     }
-    
-    //Check the answer
-checkAnswer = answer => {
-    console.log(answer)
-    this.setState({
-        userAnswer: answer,
-        disabled:false
-    })
-}
 
-componentDidUpdate(_prevProps, prevState) {
-    const{currentIndex} = this.state;
-    if(this.state.currentIndex !== prevState.currentIndex) {
-        this.setState(() => {
-            return {
-                disabled: true,
-                question: QuizData[currentIndex].question,
-                options: QuizData[currentIndex].options,
-                answer: QuizData[currentIndex].answer
-            }
-        });
+    if (this.state.currentIndex === QuizData.length - 1) {
+      this.setState({
+        quizEnd: true,
+      });
     }
-}
-
-finishHandler =() => {
-    if(this.state.currentIndex === QuizData.length -1) {
-        this.setState({
-            quizEnd:true
-        })
+  };
+  render() {
+    const { question, options, currentIndex, userAnswer, quizEnd } = this.state;
+    if (quizEnd) {
+      return (
+        <div>
+          <h1>Game Over. Final score is {this.state.score} points</h1>
+          <p>The correct Answers for the quiz are</p>
+          <ul>
+            {QuizData.map((item, index) => (
+              <li className="ui floating message options" key={index}>
+                {item.answer}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
     }
-}
-    render() {
-    const{question, options, currentIndex, userAnswer, quizEnd, score} = this.state
-    if(quizEnd) {
-        return (
-            <div>
-                <h1>Game Over. Final score is {score} points</h1>
-                <p>The correct Answers for the quiz are</p>
-                <ul>
-                    {QuizData.map((item, index) => (
-                        <li className='ui floating message options'
-                            key={index}>
-                                {item.answer}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )
-    } 
 
     return (
       <div>
-          <h2>{question}</h2>
-          <span>{`Question ${currentIndex + 1} of ${QuizData.length}`}</span>
-          {
-              options.map(option => 
-                <p key = {option.index} className={`options ${userAnswer === option? "selected" : null}`} 
-               onClick = {() => this.checkAnswer(option)}
-               > 
-                {option}
-                </p>
-              )
-          }
-        {currentIndex < QuizData.length -1 && 
-            <button disabled = {this.state.disabled} onClick={this.nextQuestionHandler}>
-                Next Question
-            </button>}
-            {currentIndex === QuizData.length -1 && 
-            <button onClick={this.finishHandler} disabled = {this.state.disabled} >
-                Finish
-            </button>}
-
+        <h2>{question}</h2>
+        <span>{`Question ${currentIndex + 1} of ${QuizData.length}`}</span>
+        {options.map((option) => (
+          <p
+            key={option.id}
+            className={`options ${userAnswer === option ? "selected" : null}`}
+            onClick={() => this.checkAnswer(option)}
+          >
+            {option}
+          </p>
+        ))}
+        {currentIndex < QuizData.length - 1 && (
+          <button
+            disabled={this.state.disabled}
+            onClick={this.nextQuestionHandler}
+          >
+            Next Question
+          </button>
+        )}
+        {currentIndex === QuizData.length - 1 && (
+          <button onClick={this.finishHandler} disabled={this.state.disabled}>
+            Finish
+          </button>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Quiz
+export default Quiz;
--- src/QuizData.js
+++ src/QuizData.js
@@ -1,32 +1,32 @@
 export const QuizData = [
     {
-        id: 1,
+        id: 0,
         question: `What does HTML stand for?`,
         options: [`Hyperlink Mark Logistics`, `Hypertext Markup Language`, `Hypertext Mark Language`, `Hyperlink Markup Logic`],
         answer: `Hypertext Markup Language`
     },
     {
-        id: 2,
+        id: 1,
         question: `What does OS stand for?`,
-        options: [`Operating System`, `Operative System`, `Operating Solutions`, `Operative Solutions`],
+        options: [`Operative Solutions`, `Operative System`, `Operating Solutions`, `Operating System`],
         answer: `Operating System`
     },
     {
-        id: 3,
+        id: 2,
         question: `What does HTTP stand for?`,
         options: [`Hypertext Transmit Protocol`, `Hypertext Transfer Prosystems`, `Hypertext Transfer Problem`, `Hypertext Transfer Protocol`],
         answer: `Hypertext Transfer Protocol`
     },
     {
-        id: 4,
+        id: 3,
         question: `What does RAM stand for?`,
         options: [`Randomized Access Memory`, `Random Access Memory`, `Random Active Memo`, `Random Activity Memory`],
         answer: `Random Access Memory`
     },
     {
-        id: 5,
+        id: 4,
         question: `What does SSD stand for in regards to computer storage?`,
-        options: [`Solid Static Drive`, `Solid State Drive`, `Solid Statistical Drive`, `Solid Stat Driver`],
+        options: [`Solid Static Drive`, `Solid State Drive`, `Solid Statistical Drive`, `Solid State Driver`],
         answer: `Solid State Drive`
     },
 ]
